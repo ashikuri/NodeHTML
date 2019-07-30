a94f8dd464a31a03f75f4f76be093d4206aaa37c
ashikuri <53426441+ashikuri@users.noreply.github.com>
2019-07-30 21:07:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,96 @@
-class NodeHTMLElement{
-  constructor(tag){
-    this.id = ""
-    this.className = ""
-    this.style = {}
-    this.innerHTML = ""
-    this.localName = tag
+let htmlVoidElements = require("./html-void.json");
+let htmlValueElements = ["input", "textarea"];
+let attrs = {};
+class NodeHTMLElement {
+  constructor(tag) {
+    this.id = "";
+    this.className = "";
+    this.style = {};
+    if (!!htmlValueElements.find(v => v == tag)) this.value = "";
+    else this.innerHTML = "";
+    this.localName = tag;
+    this.hidden = false;
   }
-  get classList(){
-    let that = this
+  get classList() {
+    let that = this;
     return {
-      add(...items){
-        items.forEach((item) =>{
-          if(!this.contains(item)) that.className += " " + item
-        })
+      add(...items) {
+        items.forEach(item => {
+          if (!this.contains(item)) that.className += " " + item;
+        });
       },
-      remove(...items){
-        items.forEach((item) =>{
-          if(this.contains(item)) that.className = that.className.replace(" " + item, "")
-        })
+      remove(...items) {
+        items.forEach(item => {
+          if (this.contains(item))
+            that.className = that.className.replace(" " + item, "");
+        });
       },
-      toggle(...items){
-        items.forEach((item) =>{
-          if(!this.contains(item)) that.className += " " + item
-          else that.className = that.className.replace(" " + item, "")
-        })
+      toggle(...items) {
+        items.forEach(item => {
+          if (!this.contains(item)) that.className += " " + item;
+          else that.className = that.className.replace(" " + item, "");
+        });
       },
-      contains(item){
-        return that.className.includes(item)
-      }
+      contains(item) {return that.className.includes(item);}
+    };
+  }
+  get outerHTML() {
+    let style = Object.keys(this.style);
+    let styleArg =
+      style.length > 0
+        ? ' style="' +
+          style
+            .map(
+              n =>
+                n.replace(/[A-Z]/g, "-$1").toLowerCase() + ":" + this.style[n]
+            )
+            .join("") +
+          '"'
+        : "";
+    let classArg = !!this.className
+      ? ' class="' + this.className.slice(1) + '"'
+      : "";
+    let valueArg = !!this.value ? ' value="' + this.value + '"' : "";
+    let idArg = !!this.id ? ' id="' + this.id + '"' : "";
+    let hiddenArg = this.hidden ? " hidden" : "";
+    let otherArgs = [];
+    for (const attrName in attrs) {
+      otherArgs.push(
+        ` ${attrName}${
+          typeof attrs[attrName] === "boolean"
+            ? ""
+            : '="' + attrs[attrName] + '"'
+        }`
+      );
     }
+
+    let isVoidElement = !!htmlVoidElements.find(v => v === this.localName);
+    let isValueElement = htmlValueElements.find(v => v === this.localName);
+    if (isVoidElement)
+      return `<${this.localName}${hiddenArg}${styleArg}${classArg}${idArg}${
+        isValueElement ? valueArg : ""
+      }${otherArgs.join("")}/>`;
+    return `<${
+      this.localName
+    }${hiddenArg}${styleArg}${classArg}${idArg}${otherArgs.join("")}>${
+      isValueElement ? this.value : this.innerHTML
+    }</${this.localName}>`;
   }
-  get outerHTML(){
-    let style = Object.keys(this.style)
-    let styleArg = style.length > 0 ? " style=\"" + style.map((n) =>n.replace(/[A-Z]/g, "-$1").toLowerCase() + ":" + this.style[n]).join(";") + "\"" : ""
-    let classArg = !!this.className ? " class=\"" + this.className.slice(1) + "\"" : ""
-    let idArg = !!this.id ? " id=\"" + this.id + "\"" : ""
-    return `<${this.localName}${styleArg}${classArg}${idArg}>${this.innerHTML}</${this.localName}>`
+  appendChild(child) {
+    this.innerHTML += child.outerHTML;
+    return child;
   }
-  appendChild(child){
-    this.innerHTML += child.outerHTML
-    return child
+  cloneNode(deep) {
+    let newElement = Object.assign({}, this);
+    if (!deep) newElement.innerHTML = "";
+    return newElement;
   }
+  setAttribute(qualifiedName, value) {attrs[qualifiedName] = value;}
+  getAttribute(qualifiedName) {return attrs[qualifiedName];}
+  hasAttribute(qualifiedName) {return qualifiedName in attrs;}
+  removeAttribute(qualifiedName) {return delete attrs[qualifiedName];}
 }
-function createElement(tag){return new NodeHTMLElement(tag)}
-module.exports = {createElement}
+function createElement(tag) {
+  return new NodeHTMLElement(tag);
+}
+module.exports = { createElement };
